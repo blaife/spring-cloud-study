0d0d4f1a5ebe5f34f4a1d243dc068d0f5fd4144a
BigBlackKnife <2767026270@qq.com>
2020-10-04 23:48:30 +0800

    支付模块yml文件提交（端口，db，mybatis）

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Blaife
+ * @description 主启动类
+ * @data 2020/10/4 21:25
+ */
+
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+
+}
