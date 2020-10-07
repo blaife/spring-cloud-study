4c09cb0c9cf5aed87504bd763656259e40692d20
BigBlackKnife <2767026270@qq.com>
2020-10-07 18:17:40 +0800

    Eureka - client 入驻

--- cloud-consumer-order80/src/main/java/com/atguigu/cloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/cloud/OrderMain80.java
@@ -2,6 +2,7 @@ package com.atguigu.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Blaife
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020/10/5 17:01
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
--- cloud-provider-payment8001/src/main/java/com/atguigu/cloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/PaymentMain8001.java
@@ -2,6 +2,7 @@ package com.atguigu.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Blaife
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020/10/4 21:25
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
