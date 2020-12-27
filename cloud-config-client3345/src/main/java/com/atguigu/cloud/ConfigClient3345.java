80d6f11b3c43dea4895cde89d62d5d053888c47b
BigBlackKnife <2767026270@qq.com>
2020-12-27 16:27:25 +0800

    Config - 配置中心

---
+++ cloud-config-client3345/src/main/java/com/atguigu/cloud/ConfigClient3345.java
@@ -0,0 +1,18 @@
+package com.atguigu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Blaife
+ * @description TODO
+ * @date 2020/12/26 19:07
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClient3345 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient3345.class, args);
+    }
+}
---
+++ cloud-config-client3345/src/main/java/com/atguigu/cloud/controller/ConfigController.java
@@ -0,0 +1,25 @@
+package com.atguigu.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Blaife
+ * @description TODO
+ * @date 2020/12/26 19:13
+ */
+@RestController
+@RefreshScope
+public class ConfigController {
+
+    @Value("${word}")
+    private String word;
+
+    @GetMapping("/getWord")
+    public String getWord() {
+        return word;
+    }
+
+}
