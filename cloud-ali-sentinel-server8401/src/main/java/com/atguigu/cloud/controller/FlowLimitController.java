ea3a58cf6bbfb8a1c9048417372923d66e927965
BigBlackKnife <2767026270@qq.com>
2021-03-27 17:00:36 +0800

    Sentinel - 添加测试用例

--- cloud-ali-sentinel-server8401/src/main/java/com/atguigu/cloud/controller/FlowLimitController.java
+++ cloud-ali-sentinel-server8401/src/main/java/com/atguigu/cloud/controller/FlowLimitController.java
@@ -1,8 +1,12 @@
 package com.atguigu.cloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -13,7 +17,7 @@ import java.util.concurrent.TimeUnit;
  * @date 2021/3/13 16:41
  */
 @RestController
-@RequestMapping("/sentinel")
+@Slf4j
 public class FlowLimitController {
 
     @GetMapping("/testA")
@@ -23,11 +27,36 @@ public class FlowLimitController {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        return Thread.currentThread().getName() + ";A------------";
+        return "--------------TestA";
     }
     @GetMapping("/testB")
     public String testB() {
-        return "B------------";
+        log.info(Thread.currentThread().getName() + "\t" + "-----TestB");
+        return "--------------TestB";
+    }
+
+    @GetMapping("/testD")
+    public String testD() {
+       /* try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+        log.info("-------TestD");
+        int age = 10 / 0;
+        return "--------------TestD";
+    }
+
+    @GetMapping("/testHotkey")
+    @SentinelResource(value = "testHotkey", blockHandler = "deal_testHotkey")
+    public String testHotkey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        return "--------------testHotkey";
+    }
+
+
+    public String deal_testHotkey(String p1, String p2, BlockException blockException) {
+        return "--------------deal_testHotkey";
+        // sentinel默认提示： Blocked by Sentinel (flow limiting);
     }
 }
